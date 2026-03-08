48172b14a725e248501277b922f8862b1beb7bce
Mariano Aloi <mariano@aloi.com.br>
2026-03-08 13:45:31 +0100

    initial compra

--- fetch_and_save.py
+++ fetch_and_save.py
@@ -1,4 +1,5 @@
 
+from enum import Enum
 import os
 import pymongo
 import requests
@@ -7,23 +8,30 @@ from pymongo import MongoClient,  UpdateOne
 
 from listing_fetcher import ListingFetcher
 
+class ImmobiliareType(Enum):
+    AFFITTO = 'a'
+    COMPRA = 'c'
+
 # --- Configuration ---
 URL_COMPRA_UD = "https://www.immobiliare.it/api-next/search-list/listings/?fkRegione=fri&idProvincia=UD&idComune=6437&idNazione=IT&idContratto=1&idCategoria=1&prezzoMassimo=200000&__lang=it&minLat=46.012105&maxLat=46.106327&minLng=13.143768&maxLng=13.347702&pag=1&paramsCount=5&path=%2Fvendita-case%2Fudine%2F"
 URL_TS="https://www.immobiliare.it/api-next/search-list/listings/?fkRegione=fri&idProvincia=TS&idComune=6307&idNazione=IT&prezzoMassimo=1200&__lang=it&idContratto=2&idCategoria=1&pag=1&paramsCount=0&path=%2Faffitto-case%2Ftrieste%2F"
 URL = "https://www.immobiliare.it/api-next/search-list/listings/?fkRegione=fri&idProvincia=UD&idNazione=IT&idContratto=2&idCategoria=1&prezzoMassimo=1200&__lang=it&minLat=46.048872&maxLat=46.07978&minLng=13.189259&maxLng=13.273544&pag=1&paramsCount=5&path=%2Faffitto-case%2Fudine-provincia%2F"
 MONGO_URI =  "mongodb+srv://cluster0.7qska.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "udine"
-COLLECTION_NAME = "affito"
+COLLECTION_AFFITO = "affito"
+COLLECTION_COMPRA = "compra"
 COLLECTION_PRIMARYFEATURES = "primaryFeatures"
 
-def compare_and_sync(collection, results):
+def compare_and_sync(collection, results, type: ImmobiliareType ):
     """
     Compares fetched results with the collection and syncs the data.
     - Inserts new documents.
     - Removes documents that are no longer in the results.
     - Updates existing documents.
     """
-    print("Comparing and synchronizing data... ",len(results), " items fetched.")
+    print("Comparing and synchronizing data... ",len(results), " items fetched. Type:", type.value)
+    
+    results = [ {**r, "type": type.value} for r in results]
 
     results_with_id = [r for r in results if r.get("_id")]
     if len(results_with_id) != len(results):
@@ -32,7 +40,7 @@ def compare_and_sync(collection, results):
     fetched_ids = {r["_id"] for r in results_with_id}
     
     # Find and remove documents that are in the DB but not in the latest fetch
-    existing_ids = {doc["_id"] for doc in collection.find({"deleted": {"$exists": False}}, {"_id": 1})}
+    existing_ids = {doc["_id"] for doc in collection.find({ "type": type.value, "deleted": {"$exists": False}}, {"_id": 1})}
     to_remove_ids = existing_ids - fetched_ids
     
     if to_remove_ids:
@@ -40,7 +48,7 @@ def compare_and_sync(collection, results):
         collection.update_many({"_id": {"$in": list(to_remove_ids)}},{"$set": {"deleted": True}})
 
     
-    items = collection.find({"deleted":{"$exists":False}})
+    items = collection.find({ "type": type.value, "deleted":{"$exists":False}})
     items = {item["realEstate"]["id"]:item for item in items}
 
     results_with_id = [ r for r in results_with_id 
@@ -78,15 +86,15 @@ def compare_and_sync(collection, results):
            
     print("Synchronization complete.")
 
-def auto_choice(db):
+def auto_choice(db, type):
     """
     Automatically selects listings based on predefined criteria.
     """
     print("Running auto-choice selection...")
-    ids = [int(doc["_id"]) for doc in db[COLLECTION_PRIMARYFEATURES].find({"Accesso_per_disabili":0},{"_id":1})]
+    ids = [int(doc["_id"]) for doc in db[COLLECTION_PRIMARYFEATURES].find({"Accesso_per_disabili":0, "type": type},{"_id":1})]
     if not ids:
         return
-    result = db[COLLECTION_NAME].update_many({
+    result = db[COLLECTION_AFFITO].update_many({
                         "deleted" : {"$exists":False},
                         "stateMaloi":{"$exists":False},
                         "_id":{"$in":ids}
@@ -94,6 +102,8 @@ def auto_choice(db):
                                              ,                                
                                              {"$set":{"stateMaloi":0}})
     print(f"Auto-choice selection complete. {result.modified_count} documents updated.")
+    
+add_type = lambda r,t: {**r, "type":t}
 
 def fetch_data_and_save_to_mongo():
     """
@@ -109,7 +119,7 @@ def fetch_data_and_save_to_mongo():
         results = ListingFetcher(URL).fetch_all_listings()
 
         
-        print(f"Fetching data from URL: {URL_TS}")
+        # print(f"Fetching data from URL: {URL_TS}")
         # results += ListingFetcher(URL_TS).fetch_all_listings()
 
         if not results:
@@ -125,12 +135,22 @@ def fetch_data_and_save_to_mongo():
                      tlsCertificateKeyFile= os.path.dirname(__file__) + '/X509-cert-2864290664025085959.pem',
                      server_api=pymongo.server_api.ServerApi('1'))
         db = client[DATABASE_NAME]
-        collection = db[COLLECTION_NAME]
+        collection = db[COLLECTION_AFFITO]
         print("MongoDB connection successful.")
 
         # --- Compare and Sync Data ---
-        compare_and_sync(collection, results)
-        auto_choice(db)
+        compare_and_sync(collection, results , ImmobiliareType.AFFITTO)
+        auto_choice(db, ImmobiliareType.AFFITTO.value)
+        
+        
+
+        print(f"Fetching data from URL: {URL_COMPRA_UD}")
+        results = ListingFetcher(URL_COMPRA_UD).fetch_all_listings()
+        
+        # --- Define type ---
+
+        # --- Compra collection ---
+        compare_and_sync(db[COLLECTION_AFFITO], results , ImmobiliareType.COMPRA)
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while fetching data: {e}")
